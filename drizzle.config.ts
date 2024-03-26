d12a090c8f164b4dc54900afa06473c3319ad514
cantdocpp <cihuyman95@gmail.com>
2024-03-26 17:46:52 +0700

    configure drizzle studio

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,14 @@
+require("dotenv").config({ path: ".env.local" });
+import { env } from "./config";
 import type { Config } from "drizzle-kit";
+
+const connectionString = `postgresql://${env.DB_USER}:${env.DB_PASSWORD}@${env.DB_HOST}:${env.DB_PORT}/${env.DB_NAME}`;
 
 export default {
   schema: "./database/schema.ts",
   out: "./drizzle",
   driver: "pg",
+  dbCredentials: {
+    connectionString: connectionString,
+  },
 } satisfies Config;
